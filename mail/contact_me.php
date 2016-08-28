2c75c91eee4431602e466caa174c185f1a2d3462
rajuathokpam <raju.athokpam@gmail.com>
2016-08-28 12:26:23 +0530

    'commiting'

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,11 +16,30 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create the email and send the message
-$to = 'raju.athokpam@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'service@itservzweb.appspotmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";
-mail($to,$email_subject,$email_body,$headers);
+// $headers = "From: noreply@itservz.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+// $headers .= "Reply-To: $email_address";
+$headers = "From: Persone<" . strip_tags("noreply@itservz.com") . "> \r\n";
+$headers .= "Reply-To: ". $email_address . "\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+//mail($to,$email_subject,$email_body,$headers);
 return true;
+
+
+use google\appengine\api\mail\Message;
+try {
+    $message = new Message();
+    $message->setSender($email_address);
+    $message->addTo($to);
+    $message->setSubject($email_subject);
+    $message->setTextBody($email_body);
+    //$message->headers($headers)
+    $message->send();
+    echo 'Your message has been sent.'
+} catch (InvalidArgumentException $e) {
+    echo 'There was an error';
+}
 ?>
